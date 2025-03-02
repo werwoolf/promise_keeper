c3f461e668ee0da212177ad1e2162e1651c55f1a
Serhii Dusyk <sd1996sdsd@gmail.com>
2025-03-02 16:17:05 +0200

    wip: task creation

--- programs/promise_keeper/src/lib.rs
+++ programs/promise_keeper/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
+use anchor_lang::prelude::borsh::{BorshDeserialize, BorshSerialize};
 
 declare_id!("ARKDUPvSk7fVmY676dLctbqDfncxy6SPiTVhy8zJabCC");
 
@@ -7,66 +8,96 @@ declare_id!("ARKDUPvSk7fVmY676dLctbqDfncxy6SPiTVhy8zJabCC");
 pub mod promise_keeper {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let mut counter = &mut ctx.accounts.counter;
-        let bump = ctx.bumps.counter;
+    pub fn create_task(
+        ctx: Context<CreateTask>,
+        name: String,
+        description: String,
+        time_to_solve_s: u32,
+    ) -> Result<()> {
+        let mut task = &mut ctx.accounts.task;
+        let bump = ctx.bumps.task;
 
-        **counter = Counter {
-            authority: *ctx.accounts.authority.key,
-            count: 0,
-            bump,
+        **task = Task {
+            name,
+            description,
+            due_date: None,
+            time_to_solve_s: 259_200,
+            user_id: None,
+            img_proof_hash: None,
+            status: TaskStatus::Pending,
         };
 
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> Result<()> {
-        require_keys_eq!(
-            ctx.accounts.authority.key(),
-            ctx.accounts.counter.authority,
-            ErrorCode::Unauthorized
-        );
-
-        ctx.accounts.counter.count += 1;
-        Ok(())
-    }
+    // pub fn increment(ctx: Context<Increment>) -> Result<()> {
+    //     require_keys_eq!(
+    //         ctx.accounts.authority.key(),
+    //         ctx.accounts.counter.authority,
+    //         ErrorCode::Unauthorized
+    //     );
+    //
+    //     ctx.accounts.counter.count += 1;
+    //     Ok(())
+    // }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+#[instruction(name: String)]
+#[instruction(description: String)]
+#[instruction(time_to_solve_s: u32)]
+pub struct CreateTask<'info> {
     #[account(
         init,
         payer = authority,
-        space = Counter::SIZE,
-        seeds = [b"counter"],
+        space = Task::SIZE,
+        seeds = [b"task", name.as_bytes()],
         bump
     )]
-    counter: Account<'info, Counter>,
+    task: Account<'info, Task>,
     #[account(mut)]
     authority: Signer<'info>,
     system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Increment<'info> {
-    #[account(
-        mut,
-        seeds = [b"counter"],
-        bump = counter.bump
-    )]
-    counter: Account<'info, Counter>,
-    authority: Signer<'info>,
-}
-
 #[account]
-pub struct Counter {
-    pub authority: Pubkey,
-    pub count: u64,
-    pub bump: u8,
+#[derive(InitSpace)]
+pub struct Task {
+    #[max_len(10)]
+    name: String,
+    #[max_len(100)]
+    description: String,
+    #[max_len(10)]
+    due_date: Option<u64>,
+    #[max_len(10)]
+    time_to_solve_s: u32,
+    #[max_len(10)]
+    user_id: Option<String>,
+    #[max_len(10)]
+    img_proof_hash: Option<String>,
+    status: TaskStatus,
 }
 
-impl Counter {
-    pub const SIZE: usize = 8 + 32 + 8 + 1;
+#[derive(AnchorSerialize, AnchorDeserialize, InitSpace, Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub enum TaskStatus {
+    Pending,
+    InProgress,
+    Voting,
+    Stale,
+    Success,
+    Fail,
+}
+
+impl Task {
+    pub const SIZE: usize = 8 + // discriminator
+        4 + 10 + // name: length prefix (4) + max length (10)
+        4 + 100 + // description: length prefix (4) + max length (100)
+        1 + 8 + // due_date: 1 byte for Option + 8 bytes for u64
+        4 + // time_to_solve_s: u32
+        1 + 4 + 10 + // user_id: 1 byte for Option + length prefix (4) + max length (10)
+        1 + 4 + 10 + // img_proof_hash: same as above
+        1; // status: 1 byte (enum is stored as a single byte)
 }
 
 #[error_code]
@@ -75,38 +106,3 @@ pub enum ErrorCode {
     Unauthorized,
 }
 
-// #[derive(Accounts)]
-// pub struct Initialize<'info> {
-//     #[account(
-//         init,
-//         payer = signer,
-//         space = size_of::<Task>() + 8,
-//         seeds = [b"task", signer.key().as_ref()],
-//         bump
-//     )]
-//     pub task: Account<'info, Task>,
-//
-//     #[account(mut)]
-//     pub signer: Signer<'info>,
-//
-//     pub system_program: Program<'info, System>,
-// }
-//
-// #[account]
-// #[derive(InitSpace)]
-// pub struct Task {
-//     #[max_len(10)]
-//     name: String,
-//     #[max_len(10)]
-//     description: String,
-//     #[max_len(10)]
-//     due_date: String,
-//     #[max_len(10)]
-//     time_to_solve: String,
-//     #[max_len(10)]
-//     user_id: Option<String>,
-//     #[max_len(10)]
-//     img_proof_hash: Option<String>,
-//     #[max_len(10)]
-//     status: String,
-// }
--- tests/promise_keeper.ts
+++ tests/promise_keeper.ts
@@ -1,4 +1,3 @@
-import assert from "node:assert";
 import * as anchor from "@coral-xyz/anchor";
 
 describe("promise_keeper", () => {
@@ -6,43 +5,56 @@ describe("promise_keeper", () => {
 
     anchor.setProvider(provider);
 
+    // const id = Math.random().toString();
     const program = anchor.workspace.promise_keeper;
-    const counterSeed = anchor.utils.bytes.utf8.encode("counter");
+    // const counterSeed = [
+    //     anchor.utils.bytes.utf8.encode("task"),
+    //     anchor.utils.bytes.utf8.encode(id),
+    // ];
 
-    let counterPubkey;
 
-    before(async () => {
-        [counterPubkey] = await anchor.web3.PublicKey.findProgramAddress(
-            [counterSeed],
-            program.programId
-        );
-    });
+    // let counterPubkey;
+    //
+    // before(async () => {
+    //     [counterPubkey] = anchor.web3.PublicKey.findProgramAddressSync(counterSeed, program.programId);
+    // });
 
     it("Is runs the constructor", async () => {
+        const name = "Wake up 7";
+        const counterSeed = [
+            anchor.utils.bytes.utf8.encode("task"),
+            anchor.utils.bytes.utf8.encode(name),
+        ];
+        let [counterPubKey] = anchor.web3.PublicKey.findProgramAddressSync(counterSeed, program.programId);
+
         await program.methods
-            .initialize()
+            .createTask(name, "Bla bla description", 1_000_000)
             .accounts({
-                counter: counterPubkey,
+                counter: counterPubKey,
                 authority: provider.wallet.publicKey,
                 systemProgram: anchor.web3.SystemProgram.programId,
             })
             .rpc();
 
-        const counterAccount = await program.account.counter.fetch(counterPubkey);
-
-        assert.ok(counterAccount.count.eq(new anchor.BN(0)));
+        const counterAccount = await program.account.task.fetch(counterPubKey);
+        console.log(counterAccount)
+        // assert.ok(counterAccount.count.eq(new anchor.BN(0)));
     });
 
-    it("Executes a method on the program", async () => {
-        await program.methods
-            .increment()
-            .accounts({
-                counter: counterPubkey,
-                authority: provider.wallet.publicKey,
-            })
-            .rpc();
+    // it("Executes a method on the program", async () => {
+    //     await program.methods
+    //         .increment()
+    //         .accounts({
+    //             counter: counterPubkey,
+    //             authority: provider.wallet.publicKey,
+    //         })
+    //         .rpc();
+    //
+    //     const counterAccount = await program.account.counter.fetch(counterPubkey);
+    //     assert.ok(counterAccount.count.eq(new anchor.BN(1)));
+    // });
 
-        const counterAccount = await program.account.counter.fetch(counterPubkey);
-        assert.ok(counterAccount.count.eq(new anchor.BN(1)));
+    it('should all', async () => {
+        console.log(await program.account.task.all())
     });
 });
